1931c08538498ab9ac21dc624752e149c61c1c1b
Jimbo Cortes <jimbo@spuul.com>
2015-01-30 22:19:04 +0800

    set the versions

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,3 +1,13 @@
 class ImageUploader < CarrierWave::Uploader::Base
   storage :file
+
+  process :resize_to_fit => [800, 800]
+  
+  version :thumb do
+    process :resize_to_fill => [200,200]
+  end
+  
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
