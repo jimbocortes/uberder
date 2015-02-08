65f011c0583f1cce8be09edaf96773c00c57caf3
Jimbo Cortes <jimbo@spuul.com>
2015-02-08 18:44:24 +0800

    add the dropzone.js

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,29 @@
 class Property < ActiveRecord::Base
   self.inheritance_column = "kind"
-end
+
+  TYPES = %w| Apartment Room Condo House | 
+  
+  CITIES =["Cebu City",
+    "Lapu-Lapu City",
+    "Mandaue City",
+    "Carcar City",
+    "Danao City",
+    "Bogo City",
+    "Talisay City",
+    "Naga City"]
+
+  BEDROOMS =[
+    ["Studio Type", 0],
+    ["1 BR", 1],
+    ["2 BR", 2],
+    ["3 BR", 3],
+    ["4+ BR", 4]]
+
+  BATHROOMS =[
+    ["1 BR", 1],
+    ["2 BR", 2],
+    ["3 BR", 3],
+    ["4+ BR", 4],
+    ["Shared", -1],
+  ]
+ end
