b36b421b50b6f08a2da275f3a20e2f542cdaf6b1
Jimbo Cortes <jimbo@spuul.com>
2015-02-19 16:57:51 +0800

    add the sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.name = auth.info.name   # assuming the user model has a name
       user.avatar = auth.info.image # assuming the user model has an image
     end
