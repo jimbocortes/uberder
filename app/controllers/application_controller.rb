02fc083db66e7603fadc2703d055548ed85ce90d
Jimbo Cortes <jimbo@spuul.com>
2015-02-20 18:39:43 +0800

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   
-  # def after_sign_in_path_for(resource)
-  # end
+  def after_sign_in_path_for(resource)
+    if request.referer == property_owner_welcome_index_url
+      new_property_path
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 end
