684625d5698e137a45ffae2303dcc74b65bb7a69
Jimbo Cortes <jimbo@spuul.com>
2015-02-25 23:42:18 +0800

    set release to 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/ca
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 4
 
 namespace :deploy do
 
