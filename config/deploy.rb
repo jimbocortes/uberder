931131a97c5f62c8f60565ec608fb8cb23a30db3
Jimbo Cortes <jimbo@spuul.com>
2015-03-01 15:35:52 +0800

    remove linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :rbenv_ruby, "2.2.0"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
-
+  
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
@@ -37,8 +37,7 @@ set :pty, true
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-set :linked_files, %w{config/database.yml config/facebook.yml config/secrets.yml}
+# set :linked_files, %w{config/database.yml config/facebook.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
